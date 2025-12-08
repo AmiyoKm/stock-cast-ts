d30bb25e9aa288079c96bd51a96df4339322dcd3
AmiyoKm <games.amiyo@gmail.com>
2025-12-08 20:33:06 +0600

    Allow StockCast/gru model in predictions schema

--- dist/api/validators/predictions.js
+++ dist/api/validators/predictions.js
@@ -9,6 +9,10 @@ export const getPredictionsSchema = z.object({
             z.literal(15),
             z.literal(30),
         ]),
-        model: z.enum(["StockCast/seperate", "StockCast/unified"]),
+        model: z.enum([
+            "StockCast/seperate",
+            "StockCast/unified",
+            "StockCast/gru",
+        ]),
     }),
 });
